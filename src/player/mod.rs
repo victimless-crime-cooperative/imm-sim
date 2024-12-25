2469eb0ec6a19050a7116268f05a4b6579d55366
spvky <Colin@wildheartinteractive.com>
2024-12-24 21:45:04 -0500

    basic crouching, do some slides after christmas

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -8,29 +8,7 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup)
-            .add_systems(Update, read_movement_inputs);
-    }
-}
-
-fn read_movement_inputs(
-    mut commands: Commands,
-    camera_config: Res<CameraConfig>,
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    player: Single<Entity, With<Player>>,
-) {
-    let player_entity = player.into_inner();
-    let up = keyboard_input.pressed(KeyCode::KeyW);
-    let down = keyboard_input.pressed(KeyCode::KeyS);
-    let left = keyboard_input.pressed(KeyCode::KeyA);
-    let right = keyboard_input.pressed(KeyCode::KeyD);
-
-    let horizontal = right as i8 - left as i8;
-    let vertical = up as i8 - down as i8;
-
-    let direction = camera_config.interpolate(Vec2::new(horizontal as f32, vertical as f32));
-
-    if direction != Vec3::ZERO {
-        commands.trigger_targets(StandingAction::Run(direction), player_entity);
+            .add_systems(Update, (read_movement_inputs, handle_grounded));
     }
 }
 
@@ -70,6 +48,14 @@ impl Command for SpawnPlayer {
                 JumpImpulse::default(),
                 MovementAcceleration::default(),
                 LateralDamping::default(),
+                ShapeCaster::new(
+                    Collider::capsule(0.2, 1.0),
+                    Vec3::ZERO,
+                    Quat::IDENTITY,
+                    Dir3::NEG_Y,
+                )
+                .with_ignore_self(true)
+                .with_max_distance(1.0),
             ))
             .with_children(|parent| {
                 parent.spawn((
@@ -91,4 +77,43 @@ fn setup(mut commands: Commands) {
         height: 2.0,
         translation: Vec3::NEG_Z + Vec3::Y * 30.0,
     });
+}
+
+fn read_movement_inputs(
+    mut commands: Commands,
+    camera_config: Res<CameraConfig>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    player: Single<Entity, With<Player>>,
+) {
+    let player_entity = player.into_inner();
+    let up = keyboard_input.pressed(KeyCode::KeyW);
+    let down = keyboard_input.pressed(KeyCode::KeyS);
+    let left = keyboard_input.pressed(KeyCode::KeyA);
+    let right = keyboard_input.pressed(KeyCode::KeyD);
+
+    let horizontal = right as i8 - left as i8;
+    let vertical = up as i8 - down as i8;
+
+    let direction = camera_config.interpolate(Vec2::new(horizontal as f32, vertical as f32));
+
+    if direction != Vec3::ZERO {
+        commands.trigger_targets(StandingAction::Run(direction), player_entity);
+    }
+}
+
+fn handle_grounded(
+    input: Res<ButtonInput<KeyCode>>,
+    mut player: Query<(&ShapeHits, &mut PlayerState)>,
+) {
+    if let Ok((hits, mut player_state)) = player.get_single_mut() {
+        if !hits.is_empty() {
+            if input.pressed(KeyCode::KeyQ) {
+                *player_state = PlayerState::Crouching;
+            } else {
+                *player_state = PlayerState::Standing;
+            }
+        } else {
+            *player_state = PlayerState::Airborne;
+        }
+    }
 }
